812772be5c78838baf646817a56542657e2fdbd1
Pedro Chamberlain <pedrochmatos@gmail.com>
2020-08-04 12:43:38 -0300

    create migrations

---
+++ server/knexfile.ts
@@ -0,0 +1,14 @@
+import path from 'path'
+
+// Migrations - Controlam a versão do Banco de Dados
+
+module.exports = {
+    client: 'sqlite3',
+    connection: {
+        filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite')
+    },
+    migrations: {
+        directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+    },
+    useNullAsDefault: true
+}
