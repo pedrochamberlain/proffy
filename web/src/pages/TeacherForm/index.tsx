3934666e4a24506f2749b734ed4d9a97f7dd033f
Pedro Chamberlain <pedrochmatos@gmail.com>
2020-08-05 19:18:19 -0300

    create scheduleItem state machine

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Input from '../../components/Input'
 import Select from '../../components/Select'
@@ -10,6 +10,18 @@ import warningIcon from '../../assets/images/icons/warning.svg'
 import './styles.css'
 
 function TeacherForm() {
+    const [scheduleItems, setScheduleItems] = useState([
+        { week_day: 0, from: '', to: ''}
+    ])
+
+    function addNewScheduleItem() {
+        setScheduleItems([
+            ...scheduleItems,
+            { week_day: 0, from: '', to: '' }
+        ])
+    }
+
+
     return (
         <div id="page-teacher-form" className="container">
             <PageHeader 
@@ -20,7 +32,6 @@ function TeacherForm() {
             <main>
                 <fieldset>
                     <legend>Seus dados</legend>
-
                     <Input name="name" label="Nome completo" />
                     <Input name="avatar" label="Avatar" />
                     <Input name="whatsapp" label="WhatsApp" />
@@ -42,6 +53,36 @@ function TeacherForm() {
                                 { value: 'Matemática', label: 'Matemática' },
                             ]} />
                     <Input name="cost" label="Custo da sua hora por aula" />
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        Horários disponíveis
+                        <button type="button" onClick={addNewScheduleItem}>
+                            + Novo Horário
+                        </button>
+                    </legend>
+
+                    {scheduleItems.map(scheduleItem => {
+                        return (
+                            <div className="schedule-item">
+                                <Select 
+                                    name="week_day"
+                                    label="Dia da Semana" 
+                                    options={[
+                                        { value: '0', label: 'Domingo' },
+                                        { value: '1', label: 'Segunda-feira' },
+                                        { value: '2', label: 'Terça-feira' },
+                                        { value: '3', label: 'Quarta-feira' },
+                                        { value: '4', label: 'Quinta-feira' },
+                                        { value: '5', label: 'Sexta-feira' },
+                                        { value: '6', label: 'Sábado' }
+                                    ]} 
+                                />
+                                <Input name="from" label="Das" type="time"/>
+                                <Input name="to" label="Até" type="time"/>
+                            </div>
+                        )})}
                 </fieldset>
 
                 <footer>
