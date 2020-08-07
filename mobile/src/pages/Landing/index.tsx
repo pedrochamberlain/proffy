df19238b2b7da1068e1ca1db0c8cba62065c3f28
Pedro Chamberlain <pedrochmatos@gmail.com>
2020-08-07 10:19:56 -0300

    add totalconnections get request

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Image, Text, TouchableOpacity } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
+import { RectButton } from 'react-native-gesture-handler'
+
+import api from '../../services/api'
 
 import landingImg from '../../assets/images/landing.png'
 import studyIcon from '../../assets/images/icons/study.png'
 import giveClassesIcon from '../../assets/images/icons/give-classes.png'
 import heartIcon from '../../assets/images/icons/heart.png'
-import { RectButton } from 'react-native-gesture-handler'
-
 import styles from './styles'
 
 function Landing() {
     const { navigate } = useNavigation()
+    const [totalConnections, setTotalConnections] = useState(0)
+
+    useEffect(() => {
+        api.get('connections').then(res => {
+            const { total } = res.data
+
+            setTotalConnections(total)
+        })
+    })
 
     function handleNavigateToGiveClassesPage() {
         navigate('GiveClasses')
@@ -23,7 +33,7 @@ function Landing() {
 
     return (
         <View style={styles.container}>
-            <Image source={landingImg} style={styles.banner}/>
+            <Image source={landingImg} style={styles.banner} />
 
             <Text style={styles.title}>
                 Seja bem-vindo, {'\n'}
@@ -45,8 +55,8 @@ function Landing() {
             </View>
 
             <Text style={styles.totalConnections}>
-                Total de 200 conexões já realizadas {' '}
-                <Image source={heartIcon}/>
+                Total de {totalConnections} conexões já realizadas {' '}
+                <Image source={heartIcon} />
             </Text>
         </View>
     )
